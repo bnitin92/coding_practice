25651a50fcf28c6185ce37a9902650136ce05995
Nitin Bhandari <bnitin92@gmail.com>
2020-09-28 17:43:27 -0600

    LL merging

---
+++ Linked_List/LC/21. Merge Two Sorted Lists.py
@@ -0,0 +1,30 @@
+# Merging 2 sorted linked lists
+
+"""
+Merge two sorted linked lists and return it as a new sorted list.
+ The new list should be made by splicing together the nodes of the first two lists.
+
+"""
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+def mergeTwoLists(l1, l2):
+    # create a dummy node and link the nodes to that
+
+    dummy = curr = ListNode()
+
+    while l1 and l2:
+        if l1.val < l2.val:
+            curr.next = l1
+            l1 = l1.next
+        else:
+            curr.next = l2
+            l2 = l2.next
+        curr = curr.next
+
+    curr.next = l1 or l2
+
+    return dummy.next
